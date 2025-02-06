4c56f327339cdb417763b78036c9ee036fd8dccc
noah <90880131+noahsmiths@users.noreply.github.com>
2025-02-06 17:54:28 -0500

    First version

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,43 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { cors } from "@elysiajs/cors";
+import { staticPlugin } from "@elysiajs/static";
+import { mkdir } from "node:fs/promises";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const { UPLOAD_SECRET, FILE_UPLOAD_FOLDER_PATH, PORT } = process.env;
+if (UPLOAD_SECRET === undefined || FILE_UPLOAD_FOLDER_PATH === undefined || PORT === undefined) {
+  console.error(`Make sure UPLOAD_SECRET and FILE_UPLOAD_FOLDER_PATH and PORT exist in your .env file`);
+  process.exit(1);
+}
+
+await mkdir(FILE_UPLOAD_FOLDER_PATH, { recursive: true });
+
+const app = new Elysia()
+  .use(cors())
+  .use(staticPlugin({
+    assets: FILE_UPLOAD_FOLDER_PATH,
+    prefix: "files"
+  }))
+  .post("/upload", async ({ body: { file }, headers: { host, authorization } }) => {
+    if (authorization !== UPLOAD_SECRET) {
+      throw new Error("Unauthorized");
+    }
+
+    const uploadFileName = `${Bun.randomUUIDv7()}.${file.name.substring(file.name.indexOf(".") + 1)}`;
+    const uploadTarget = `${FILE_UPLOAD_FOLDER_PATH}/${uploadFileName}`;
+    await Bun.write(uploadTarget, file);
+
+    return `http://${host}/files/${uploadFileName}`;
+  }, {
+    body: t.Object({
+      file: t.File()
+    }),
+    headers: t.Object({
+      host: t.String(),
+      authorization: t.String()
+    })
+  })
+  .listen(PORT);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `File server is running at ${app.server?.hostname}:${app.server?.port}`
 );
